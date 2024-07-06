a039bc517c1807feeee5577af9e51f81a664346f
PatricioPonce28 <168014711+PatricioPonce28@users.noreply.github.com>
2024-07-05 19:06:29 -0500

    Add files via upload

---
+++ Proyecto_Final.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+ventana = Tk()
+ventana.geometry("350x550")
+ventana.title("Mi ventana")
+
+# Aquí puedes añadir más widgets y configuraciones a la ventana
+
+ventana.mainloop()  # Este es el bucle principal de Tkinter
